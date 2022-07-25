dbeda9b711cfd223224b915904d3c030ef4b8284
camillinhaa <camillla.larissa@gmail.com>
2022-07-25 17:24:05 -0300

    add componentes necessarios para o estrutura do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { HomeComponent } from './home/home.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { ItemMenuComponent } from './menu/item-menu/item-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    MenuComponent,
+    CabecalhoComponent,
+    HomeComponent,
+    CarrinhoComponent,
+    ItemMenuComponent
   ],
   imports: [
     BrowserModule,
@@ -16,3 +26,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
